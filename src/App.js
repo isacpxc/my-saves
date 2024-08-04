60babaa2b6955360f47cee94b0f6f72b4f8042e0
isacpxc <isacpxc@gmail.com>
2024-08-04 16:29:27 -0300

    resolved #2

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import { useEffect, useRef, useState } from "react";
 import "./app.css";
 import Header from "./Header";
 import "./reusable.css";
-// import axios from "axios";
+import axios from "axios";
 import dataVideos from "./data.json";
+import imgInsta from "./img/instagram.jpg";
 
 function App() {
   const [tagVerify, setTagVerify] = useState(0);
@@ -12,6 +13,19 @@ function App() {
   // function handleTagVerify() {
   //   setTagVerify(tagVerify + 1);
   // }
+
+  useEffect(() => {
+    const img = axios({
+      url: "https://www.instagram.com/p/C9zFMxnR1dr/media/?size=m",
+      headers: {},
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [ref]);
 
   function handleRef(tab) {
     const eTab = document.getElementsByClassName("link-tab");
@@ -76,9 +90,24 @@ function App() {
             <div className="item-list">
               <div
                 className="img-show"
-                style={{
-                  background: `url(https://img.youtube.com/vi/${id}/0.jpg)`,
+                onClick={(e) => {
+                  e.preventDefault();
+                  window.open(
+                    `https://www.youtube.com/watch?v=${id}`,
+                    "_blank"
+                  );
                 }}
+                style={
+                  ref === "youtube"
+                    ? {
+                        background: `url(https://img.youtube.com/vi/${id}/0.jpg)`,
+                      }
+                    : {
+                        background: `url(${imgInsta})`,
+                      }
+                  // background: `url(https://img.youtube.com/vi/${id}/0.jpg)`,
+                  // background: `url(https://www.instagram.com/p/${id}/media/?size=l)`
+                }
               ></div>
               <div className="info-show d-flex">
                 <span className="title-video">Title: {title}</span>
